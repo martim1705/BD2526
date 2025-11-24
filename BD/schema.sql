3a3f7199f0692b824a09144733c5f7facc541aa3
martim1705 <up202304944@up.pt>
2025-11-24 17:56:34 +0000

    more work

--- BD/schema.sql
+++ BD/schema.sql
@@ -1,4 +1,11 @@
-/*PRAGMA FOREIGN_KEYS = ON;
+PRAGMA FOREIGN_KEYS = ON;
+DROP TABLE IF EXISTS district;
+DROP TABLE IF EXISTS municipality;
+DROP TABLE IF EXISTS party;
+DROP TABLE IF EXISTS independent;
+DROP TABLE IF EXISTS coalition;
+DROP TABLE IF EXISTS coalition_member;
+DROP TABLE IF EXISTS party_votes;   
 -- A.
 CREATE TABLE district (
     id INTEGER PRIMARY KEY CHECK (id BETWEEN 1 AND 20),
@@ -23,66 +30,73 @@ CREATE TABLE municipality (
 
 
 -- C. 
-INSERT INTO district (id, name) VALUES
-(13, 'PORTO'); 
-*/
+--INSERT INTO district (id, name) VALUES
+--(13, 'PORTO'); 
+
 -- D. 
+--SELECT * FROM municipality; 
 
+-- E. 
 
-
-
-UPDATE district set id = 13 where id = 14; 
-
-/*CREATE TABLE party (
-    id INTEGER PRIMARY KEY,
-    shortname TEXT,
+CREATE TABLE party (
+    id INTEGER PRIMARY KEY, 
+    shortname TEXT NOT NULL UNIQUE, 
     name TEXT NOT NULL
 );
 
+
+
 CREATE TABLE independent (
-    municipality_code INTEGER PRIMARY KEY, 
-    shortname TEXT PRIMARY KEY,
-    votes INTEGER,
-    FOREIGN KEY votes REFERENCES municipality(code)
+    municipality_code INTEGER , 
+    shortname TEXT NOT NULL,
+    name TEXT NOT NULL, 
+    votes INTEGER CHECK (votes >= 0),
+    FOREIGN KEY (municipality_code) REFERENCES municipality(code) ON UPDATE CASCADE ON DELETE RESTRICT ,
+    PRIMARY KEY (municipality_code,shortname)
 );
 
 CREATE TABLE coalition (
-    municipality_code INTEGER PRIMARY KEY,
-    shortname TEXT PRIMARY KEY,
+    municipality_code INTEGER,
+    shortname TEXT,
     name TEXT,
-    FOREIGN KEY municipality_code REFERENCES municipality(code) 
+    FOREIGN KEY (municipality_code) REFERENCES municipality(code) ON UPDATE CASCADE ON DELETE RESTRICT,
+    PRIMARY KEY (municipality_code,shortname) 
 );
 
 CREATE TABLE coalition_member(
-    municipality_code INTEGER PRIMARY KEY,
-    coalition_shortname TEXT PRIMARY KEY,
-    order_number INTEGER PRIMARY KEY,
+    municipality_code INTEGER,
+    coalition_shortname TEXT,
+    order_number INTEGER,
     party_id INTEGER,
 
-    FOREIGN KEY municipality_code REFERENCES coalition(municipality_code),
-    FOREIGN KEY coalition_shortname REFERENCES coalition(shortname)
+    FOREIGN KEY (municipality_code) REFERENCES coalition(municipality_code) ON UPDATE CASCADE,
+    FOREIGN KEY (coalition_shortname) REFERENCES coalition(shortname) ON UPDATE CASCADE,
+    PRIMARY KEY (municipality_code, coalition_shortname,order_number)
 );
 
 CREATE TABLE party_votes (
-    municipality_code INTEGER PRIMARY KEY,
-    party_id INTEGER PRIMARY KEY,
+    municipality_code INTEGER,
+    party_id INTEGER,
     votes INTEGER,
-    FOREIGN KEY municipality_code REFERENCES coalition(municipality_code),
-    FOREIGN KEY coalition_shortname REFERENCES coalition(shortname)
+    FOREIGN KEY (municipality_code) REFERENCES coalition(municipality_code),
+    FOREIGN KEY (coalition_shortname) REFERENCES coalition(shortname),
+    PRIMARY KEY (municipality_code,party_id)
 );
 
 CREATE TABLE coalition_votes (
-    municipality_code INT PRIMARY KEY, 
-    coalition_shortname TEXT PRIMARY KEY,
+    municipality_code INT, 
+    coalition_shortname TEXT,
     votes INTEGER,
-    FOREIGN KEY municipality_code REFERENCES coalition(municipality_code),
-    FOREIGN KEY coalition_shortname REFERENCES coalition(shortname)
+    FOREIGN KEY (municipality_code) REFERENCES coalition(municipality_code),
+    FOREIGN KEY (coalition_shortname) REFERENCES coalition(shortname),
+    PRIMARY KEY (municipality_code, coalition_shortname)
 );
 
 CREATE TABLE independent_votes(
-    municipality_code INT PRIMARY KEY,
-    independent_shortname TEXT PRIMARY KEY,
-    votes INTEGER,
-    FOREIGN KEY municipality_code REFERENCES independent(municipality_code),
-    FOREIGN KEY independent_shortname REFERENCES independent(shortname)
-); */
+    municipality_code INTEGER,
+    independent_shortname TEXT,
+    votes INTEGER CHECK (votes >= 0),
+    FOREIGN KEY (municipality_code) REFERENCES independent(municipality_code),
+    FOREIGN KEY (independent_shortname) REFERENCES independent(shortname),
+    PRIMARY KEY (municipality_code, independent_shortname)
+); 
